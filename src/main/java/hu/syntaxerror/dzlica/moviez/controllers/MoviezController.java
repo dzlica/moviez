91bb8c514e4607ae0d20f15298bf1ccf2a21782c
Magdalena Dzindzisz <dzlica@gmail.com>
2017-11-03 23:02:44 +0100

    Moviez: add new titles

--- src/main/java/hu/syntaxerror/dzlica/moviez/controllers/MoviezController.java
+++ src/main/java/hu/syntaxerror/dzlica/moviez/controllers/MoviezController.java
@@ -1,9 +1,13 @@
 package hu.syntaxerror.dzlica.moviez.controllers;
 
+import hu.syntaxerror.dzlica.moviez.model.Moviez;
 import hu.syntaxerror.dzlica.moviez.repositories.MoviezRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,8 +19,22 @@ public class MoviezController {
 
     @RequestMapping({"/", "/list"})
     public String list(Model model) {
-        model.addAttribute("movies", moviezRepo.findAll());
+        model.addAttribute("moviez", moviezRepo.findAll());
         return "moviez";
     }
+
+    @GetMapping("/add")
+    public String addTitle(Model model) {
+        model.addAttribute("addtitle", new Moviez());
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String postTitle(@ModelAttribute Moviez moviez, Model model) {
+        moviezRepo.save(moviez);
+        model.addAttribute("moviez", moviezRepo.findAll());
+        return "moviez";
+    }
+
 
 }
